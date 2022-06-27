e1ed1fa3ea4617506fdf93991ae170353e7538de
Kate4ka007 <shlemena@mail.ru>
2022-06-27 23:42:37 +0300

    feat: add api-key

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '1c385e235cf54c4993dfb82077ca3a69', // получите свой ключ https://newsapi.org/
         });
     }
 }
