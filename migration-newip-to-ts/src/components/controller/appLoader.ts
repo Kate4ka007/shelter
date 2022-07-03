938e7a948e3de63c36f2c29840a30ebdccbcd653
Kate4ka007 <shlemena@mail.ru>
2022-07-03 20:08:49 +0300

    fix: change link

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', {
+    super('https://nodenews.herokuapp.com/', {
       apiKey: '1c385e235cf54c4993dfb82077ca3a69', // получите свой ключ https://newsapi.org/
     });
   }
