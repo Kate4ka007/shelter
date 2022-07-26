805d2745ea99060a06fc87fdf331e91dfb9d100f
Kate4ka007 <shlemena@mail.ru>
2022-07-27 00:24:44 +0300

    feat: add cars

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,17 +1,56 @@
+/* eslint-disable no-unused-vars */
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.scss';
 
-let garage: any;
+interface ICar {
+  id: number;
+  name: string;
+  color: string;
+}
+
+let garage: Array<ICar>;
 
 fetch('http://localhost:3000/garage')
   .then((response) => response.json())
-  .then((data) => {
+  .then((data: ICar[]) => {
     garage = data;
   });
 
+class Car {
+  id: number;
+
+  name: string;
+
+  color: string;
+
+  constructor(id: number, name: string, color: string) {
+    this.id = id;
+    this.name = name;
+    this.color = color;
+
+    const car = document.createElement('div');
+    car.className = 'car';
+    car.id = id.toString();
+    car.textContent = name;
+    car.style.background = color;
+    document.body.appendChild(car);
+
+    car.addEventListener('click', () => {
+      fetch(`http://localhost:3000/garage/${this.id}`, { method: 'DELETE' })
+        .then((response) => response.json())
+        .then((data: ICar[]) => {
+          console.log(data);
+        });
+      car.remove();
+    });
+  }
+}
+
 const dat = async () => {
-  console.log(garage[0].name);
+  garage.forEach((element: ICar) => {
+    const car = new Car(element.id, element.name, element.color);
+  });
 };
 
 setTimeout(() => {
